494f78c79b75f7868d48abc0e61ac5ff2d236f1a
Matthew Bokovitz <MattBokovitz@Matthews-MBP-2.fios-router.home>
2021-01-12 11:37:51 -0500

    Edit Styles

--- api/server.js
+++ api/server.js
@@ -1,14 +1,7 @@
-// write the server here and export it
-const express = require("express"); // CommonJS
-// import express from 'express'
+const express = require("express");
 const User = require("./user-model");
-const server = express(); // instantiates an express app
-server.use(express.json()); // configures the app to read the body of requests
-// build a simple endpoint
-// [GET] /
-server.get("/", (req, res) => {
-  res.json({ message: "hello world" });
-});
+const server = express();
+server.use(express.json());
 
 server.get("/api/users", (req, res) => {
   User.findAll()
@@ -16,7 +9,9 @@ server.get("/api/users", (req, res) => {
       res.status(200).json(users);
     })
     .catch((error) => {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({
+        errorMessage: "The users information could not be retrieved.",
+      });
     });
 });
 
@@ -32,24 +27,26 @@ server.get("/api/users/:id", (req, res) => {
       }
     })
     .catch((error) => {
-      res.status(500).json({ message: error.message });
+      res
+        .status(500)
+        .json({ errorMessage: "The user information could not be retrieved." });
     });
 });
 
 server.post("/api/users", async (req, res) => {
-  //   const { name, bio } = req.body;
   if (!req.body.name || !req.body.bio) {
     res
       .status(400)
       .json({ message: "Please provide name and bio for the user." });
-    // console.log(name);
-    // console.log(bio);
   } else {
     try {
       const newlyCreated = await User.create(req.body);
       res.status(201).json(newlyCreated);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({
+        errorMessage:
+          "There was an error while saving the user to the database",
+      });
     }
   }
 });
@@ -61,7 +58,7 @@ server.put("/api/users/:id", async (req, res) => {
   if (!changes.name || !changes.bio || changes.adopter_id === undefined) {
     res
       .status(400)
-      .json({ message: "Please provide name and bio for the user." });
+      .json({ errorMessage: "Please provide name and bio for the user." });
   } else {
     try {
       const updated = await User.update(id, changes);
@@ -71,13 +68,14 @@ server.put("/api/users/:id", async (req, res) => {
         res.status(200).json(updated);
       }
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res
+        .status(500)
+        .json({ errorMessage: "The user information could not be modified." });
     }
   }
 });
 
 server.delete("/api/users/:id", (req, res) => {
-  //   const { id } = req.params;
   User.delete(req.params.id)
     .then((deleted) => {
       if (!deleted) {
@@ -87,9 +85,8 @@ server.delete("/api/users/:id", (req, res) => {
       }
     })
     .catch((error) => {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ errorMessage: "The user could not be removed" });
     });
 });
-// export this server so index.js can get it
-module.exports = server; // CommonJS syntax
-// export default server // ES6 native modules syntax
+
+module.exports = server;
